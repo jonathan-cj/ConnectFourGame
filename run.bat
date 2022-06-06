cbe88a396cd72e838e5067a2dd127bfcbf887320
jonathan-cj <69293681+jonathan-cj@users.noreply.github.com>
2022-06-06 21:14:21 +0700

    edit script file

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-python main.py --row 6 --col 7 --type bvb --player_choice 1 --thinking_time 10 --verbose
+python main.py --row 6 --col 7 --type bvb --player_choice 1 --thinking_time 3 --verbose
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 main.py --is_dump --row 6 --col 7 --type bvb --player_choice 1 --thinking_time 3 --bot1 MinimaxGroup1.pkl --bot2 LocalSearchGroup1.pkl
+python3 main.py --is_dump --row 6 --col 7 --type bvb --player_choice 1 --thinking_time 3
